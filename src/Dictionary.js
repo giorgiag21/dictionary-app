82bab0edb7d40ffcf9ef6921d9b119d9a47d466d
giorgiag21 <giorgia.galbiati90@gmail.com>
2021-12-29 12:03:05 +0100

    Added photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [headword, setHeadword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${headword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001fd4acf33b06043d083dd1dce7b6e47c2";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${headword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function updateHeadword(event) {
@@ -29,6 +41,7 @@ export default function Dictionary() {
         </form>
       </div>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
